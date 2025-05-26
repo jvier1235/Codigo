3c3ff2459201bd55056a592a0f5c0c0a19723e69
jvier1235 <arciniegasosajavieraugusto@gmail.com>
2025-05-26 12:34:53 -0600

    Add files via upload

---
+++ cel1 S3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int x = 2;
+    int y = 5;
+    printf ("Antes x = %d, y = %d \n", x, y);
+    intercambio (&x, &y);
+    printf ("Despues x = %d, y = %d \n", x, y);
+    system("Pause");
+}
+intercambio(int *a, int *b) {
+    int temp;
+    temp = *b;
+    *b = *a;
+    *a = temp;
+}
