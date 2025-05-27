0df26b4e538a847158b23f188bef7bee4bf85e70
jvier1235 <arciniegasosajavieraugusto@gmail.com>
2025-05-27 07:22:57 -0600

    Add files via upload

---
+++ cel 2 S3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    char cadena[10];
+    printf("un int ocupa%d bytes \n",sizeof(int));
+    printf("un char ocupa%d bytes \n", sizeof(char));
+    printf("un float ocupa%d bytes \n",sizeof(float));
+    printf("un double ocupa%d bytes \n", sizeof(double));
+    printf("ordena ocupa%d bytes \n", sizeof(cadena));
+    return 0;
+}
