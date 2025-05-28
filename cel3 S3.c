e46e740503a36ac50d91a9aa6c31ac44ecb2e1e2
jvier1235 <arciniegasosajavieraugusto@gmail.com>
2025-05-27 22:00:21 -0600

    Add files via upload

---
+++ cel3 S3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int array[10]={1,2,3,4,5,6,7,8,9,0};
+    int len=sizeof(array)/sizeof(int);
+    printf("los bytes del arreglo son: %d\n",sizeof(array));
+    printf("cadena entero riene: %d bytes\n",sizeof(int));
+    printf("El arreglo tiene %d elementso\n",len);
+    return 0;
+}
